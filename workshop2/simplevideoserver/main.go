a8e8d35b399886315e2f8097437d2f7501176415
AniSkyWorker <holyrecycler@mail.ru>
2018-12-08 11:19:33 +0300

    add gorilla routing

--- workshop2/simplevideoserver/main.go
+++ workshop2/simplevideoserver/main.go
@@ -2,28 +2,11 @@ package main
 
 import (
 	"fmt"
+	"github.com/aniskyworker/go-course/workshop2/simplevideoserver/handlers"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/api/v1/list", func(w http.ResponseWriter, _ *http.Request) {
-		fmt.Fprint(w, `[{
-		"id": "d290f1ee-6c54-4b01-90e6-d701748f0851",
-			"name": "Black Retrospective Woman",
-			"duration": 15,
-			"thumbnail": "/content/d290f1ee-6c54-4b01-90e6-d701748f0851/screen.jpg"
-		}]`)
-	})
-
-	http.HandleFunc("/api/v1/video/d290f1ee-6c54-4b01-90e6-d701748f0851", func(w http.ResponseWriter, _ *http.Request) {
-		fmt.Fprint(w, `{
-    		"id": "d290f1ee-6c54-4b01-90e6-d701748f0851",
-    		"name": "Black Retrospective Woman",
-    		"duration": 15,
-			"thumbnail":"/content/d290f1ee-6c54-4b01-90e6-d701748f0851/screen.jpg",
-			"url":"/content/d290f1ee-6c54-4b01-90e6-d701748f0851/index.mp4" 
-		}`)
-	})
-
-	http.ListenAndServe(":8000", nil)
+	router := handlers.Router()
+	fmt.Println(http.ListenAndServe(":8000", router))
 }
