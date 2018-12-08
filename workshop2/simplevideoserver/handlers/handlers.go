40ca008d11378065746bb27f7c9fa9d64c579767
AniSkyWorker <holyrecycler@mail.ru>
2018-12-08 11:23:49 +0300

    fix routing paths

--- workshop2/simplevideoserver/handlers/handlers.go
+++ workshop2/simplevideoserver/handlers/handlers.go
@@ -8,7 +8,7 @@ import (
 func Router() *mux.Router {
 	r := mux.NewRouter()
 	s := r.PathPrefix("/api/v1").Subrouter()
-	s.HandleFunc("/api/v1/list", getVideoList).Methods(http.MethodGet)
-	s.HandleFunc("/api/v1/video/d290f1ee-6c54-4b01-90e6-d701748f0851", getVideo).Methods(http.MethodGet)
+	s.HandleFunc("/list", getVideoList).Methods(http.MethodGet)
+	s.HandleFunc("/video/d290f1ee-6c54-4b01-90e6-d701748f0851", getVideo).Methods(http.MethodGet)
 	return r
 }
