4d64cfe513c59d77968d0361882577fe3edec06d
AniSkyWorker <holyrecycler@mail.ru>
2018-12-08 11:38:51 +0300

    add request logging

--- workshop2/simplevideoserver/handlers/handlers.go
+++ workshop2/simplevideoserver/handlers/handlers.go
@@ -1,14 +1,28 @@
 package handlers
 
+import log "github.com/sirupsen/logrus"
+
 import (
 	"github.com/gorilla/mux"
 	"net/http"
 )
 
-func Router() *mux.Router {
+func Router() http.Handler {
 	r := mux.NewRouter()
 	s := r.PathPrefix("/api/v1").Subrouter()
 	s.HandleFunc("/list", getVideoList).Methods(http.MethodGet)
 	s.HandleFunc("/video/d290f1ee-6c54-4b01-90e6-d701748f0851", getVideo).Methods(http.MethodGet)
-	return r
+	return logMiddleware(r)
+}
+
+func logMiddleware(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.WithFields(log.Fields{
+			"method":     r.Method,
+			"url":        r.URL,
+			"remoteAddr": r.RemoteAddr,
+			"userAgent":  r.UserAgent(),
+		}).Info("got a new request")
+		h.ServeHTTP(w, r)
+	})
 }
--- workshop2/simplevideoserver/main.go
+++ workshop2/simplevideoserver/main.go
@@ -1,12 +1,24 @@
 package main
 
+import log "github.com/sirupsen/logrus"
+
 import (
 	"fmt"
 	"github.com/aniskyworker/go-course/workshop2/simplevideoserver/handlers"
 	"net/http"
+	"os"
 )
 
 func main() {
+	log.SetFormatter(&log.JSONFormatter{})
+	file, err := os.OpenFile("my.log", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0666)
+	if err == nil {
+		log.SetOutput(file)
+		defer file.Close()
+	}
+
+	serverUrl := ":8000"
+	log.WithFields(log.Fields{"url": serverUrl}).Info("starting the server")
 	router := handlers.Router()
 	fmt.Println(http.ListenAndServe(":8000", router))
 }
