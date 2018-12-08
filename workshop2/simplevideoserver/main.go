9578fc706d80055ccf9240ce473f30e3bd388dc4
AniSkyWorker <holyrecycler@mail.ru>
2018-12-08 10:59:05 +0300

    fix typo in video title

--- workshop2/simplevideoserver/main.go
+++ workshop2/simplevideoserver/main.go
@@ -9,7 +9,7 @@ func main() {
 	http.HandleFunc("/api/v1/list", func(w http.ResponseWriter, _ *http.Request) {
 		fmt.Fprint(w, `[{
 		"id": "d290f1ee-6c54-4b01-90e6-d701748f0851",
-			"name": "Black Retrospetive Woman",
+			"name": "Black Retrospective Woman",
 			"duration": 15,
 			"thumbnail": "/content/d290f1ee-6c54-4b01-90e6-d701748f0851/screen.jpg"
 		}]`)
@@ -18,7 +18,7 @@ func main() {
 	http.HandleFunc("/api/v1/video/d290f1ee-6c54-4b01-90e6-d701748f0851", func(w http.ResponseWriter, _ *http.Request) {
 		fmt.Fprint(w, `{
     		"id": "d290f1ee-6c54-4b01-90e6-d701748f0851",
-    		"name": "Black Retrospetive Woman",
+    		"name": "Black Retrospective Woman",
     		"duration": 15,
 			"thumbnail":"/content/d290f1ee-6c54-4b01-90e6-d701748f0851/screen.jpg",
 			"url":"/content/d290f1ee-6c54-4b01-90e6-d701748f0851/index.mp4" 
